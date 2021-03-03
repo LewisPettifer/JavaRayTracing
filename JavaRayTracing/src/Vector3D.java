b46946f66a9c52871eec62d6ac028b328ae8e071
Lewis <lewis,pettifer@gmail.com>
2021-03-03 15:23:41 +0000

    Started vector3d class

---
+++ JavaRayTracing/src/Vector3D.java
@@ -0,0 +1,20 @@
+
+public class Vector3D {
+	
+	private float x;
+	private float y;
+	private float z;
+
+	public Vector3D() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	
+	public Vector3D(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+}
