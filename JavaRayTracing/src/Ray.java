06f90b1cd41c090a355298f97f1ef40ea4ca1d13
Lewis <lewis,pettifer@gmail.com>
2021-03-07 17:09:38 +0000

    Created getters and setters

--- JavaRayTracing/src/Ray.java
+++ JavaRayTracing/src/Ray.java
@@ -6,23 +6,39 @@ public class Ray {
 	private double tMax = Double.MAX_VALUE;
 	
 	public Ray() {
-		this.origin = new Vector3D(0.0, 0.0, 0.0);
-		this.direction = new Vector3D();
+		this.setOrigin(new Vector3D(0.0, 0.0, 0.0));
+		this.setDirection(new Vector3D());
 	}
 	
 	public Ray(Vector3D origin, Vector3D direction) {
-		this.origin = origin;
-		this.direction = direction;
+		this.setOrigin(origin);
+		this.setDirection(direction);
 	}
 	
 	public Ray(Vector3D origin, Vector3D direction, double tMax) {
-		this.origin = origin;
-		this.direction = direction;
+		this.setOrigin(origin);
+		this.setDirection(direction);
 		this.tMax = tMax;
 	}
 	
+	public Vector3D getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Vector3D origin) {
+		this.origin = origin;
+	}
+
+	public Vector3D getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Vector3D direction) {
+		this.direction = direction;
+	}
+
 	public Vector3D calculateT(double t) {
-		return (origin.add(direction)).multi(t);
+		return (getOrigin().add(getDirection())).multi(t);
 	}
 	
 }
