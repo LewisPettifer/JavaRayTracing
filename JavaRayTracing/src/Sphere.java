88d40fdbbb56b397129f3ef1b3bfede0bb103f90
Lewis <lewis,pettifer@gmail.com>
2021-03-07 17:27:02 +0000

    Created intersect function to calculate if a ray intersects the sphere

--- JavaRayTracing/src/Sphere.java
+++ JavaRayTracing/src/Sphere.java
@@ -30,4 +30,39 @@ public class Sphere {
 		this.g = g;
 		this.b = b;
 	}
+	
+	public Boolean intersect(Intersection inter) {
+	
+		//local ray
+		Ray localRay = inter.getRay();
+		localRay.setOrigin(localRay.getOrigin().sub(centre));
+		
+		//calculate quad
+		double a = localRay.getDirection().length();
+		double b = 2 * localRay.getDirection().dotProduct(localRay.getOrigin());
+		double c = localRay.getOrigin().length() - (radius * radius);
+		
+		//check if we intersect
+		double intersect = (b*b) - 4 * a * c;
+		if (intersect < 0.0) {
+			return false;
+		}
+		
+		//points of intersection
+		double t1 = (-b - Math.sqrt(intersect)) / (2 * a);
+		double t2 = (-b + Math.sqrt(intersect)) / (2 * a);
+		
+		//test if either points are valid
+		if (t1 > 0.0001 && t1 < inter.getT()) {
+			inter.setT(t1);
+		} else if (t2 > 0.0001 && t2 < inter.getT()) {
+			inter.setT(t2);
+		} else {
+			return false; //Both failed
+		}
+		
+		return true;
+	}
+	
+	
 }
