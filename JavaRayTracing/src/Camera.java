5e25cf6aaf90d63c060b0ce1c8095ffbd8ea8a6f
Lewis <lewis,pettifer@gmail.com>
2021-03-10 16:14:41 +0000

    Created constructor

--- JavaRayTracing/src/Camera.java
+++ JavaRayTracing/src/Camera.java
@@ -1,8 +1,20 @@
 
 public class Camera {
 	
-	public Camera() {
+	private Vector3D origin;
+	private Vector3D forward;
+	private Vector3D up;
+	private Vector3D right;
+	double h;
+	double w;
+	
+	public Camera(Vector3D origin, Vector3D target, Vector3D up2, double fov, double aspectRatio) {
+		this.origin = origin;
+		this.right = up2.crossProduct(forward).normalised();
+		this.up = right.crossProduct(forward);
 		
+		h = Math.tan(fov);
+		w = h * aspectRatio;
 	}
-
+	
 }
