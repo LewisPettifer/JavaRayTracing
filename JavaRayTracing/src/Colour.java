4006ba7c566f965cf15d4a84b0a6ae1aa4b6586b
Lewis <lewis,pettifer@gmail.com>
2021-03-15 17:45:27 +0000

    Added colour class

---
+++ JavaRayTracing/src/Colour.java
@@ -0,0 +1,33 @@
+
+public class Colour {
+
+	private int r;
+	private int g;
+	private int b;
+	
+	public Colour() {
+		this.r = 255;
+		this.g = 0;
+		this.b = 0;
+	}
+	
+	public Colour(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public int getB() {
+		return b;
+	}
+	
+	
+}
