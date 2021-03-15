4cb9f9394b21cc6008cb4315dd03b500c7776d23
Lewis <lewis,pettifer@gmail.com>
2021-03-15 17:47:40 +0000

    Implemented colour

--- JavaRayTracing/src/Sphere.java
+++ JavaRayTracing/src/Sphere.java
@@ -3,32 +3,30 @@ public class Sphere {
 
 	private Vector3D centre;
 	private double radius;
-	private int r;
-	private int g;
-	private int b;
+	private Colour colour;
 	
 	public Sphere() {
 		this.centre = new Vector3D();
 		this.radius = 1.0;
-		this.r = 255;
-		this.g = 0;
-		this.b = 0;
+		this.colour = new Colour();
 	}
 	
 	public Sphere(Vector3D centre, double radius) {
 		this.centre = centre;
 		this.radius = radius;
-		this.r = 255;
-		this.g = 0;
-		this.b = 0;
+		this.colour = new Colour();
 	}
 	
 	public Sphere(Vector3D centre, double radius, int r, int g, int b) {
 		this.centre = centre;
 		this.radius = radius;
-		this.r = r;
-		this.g = g;
-		this.b = b;
+		this.colour = new Colour(r, g, b);
+	}
+	
+	public Sphere(Vector3D centre, double radius, Colour colour) {
+		this.centre = centre;
+		this.radius = radius;
+		this.colour = colour;
 	}
 	
 	public Vector3D getCentre() {
