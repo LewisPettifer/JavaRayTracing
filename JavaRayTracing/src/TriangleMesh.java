da48f1933cfe75dd9add390cb1e4a66c4e31a284
LewisPettifer <lewis.pettifer1@gmail.com>
2021-03-20 00:15:41 +0000

    Started implementing Triangle meshes

---
+++ JavaRayTracing/src/TriangleMesh.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TriangleMesh extends Shape{
+
+	private ArrayList<Triangle> triangles;
+	
+	public TriangleMesh(ArrayList<Triangle> t) {
+		this.triangles = t;
+	}
+	
+	@Override
+	public Boolean intersect(Intersection inter) {
+		
+		int triangleCount = triangles.size();
+		for(int i = 0; i < triangleCount; i++) {
+			if (triangles.get(i).intersect(inter)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+}
